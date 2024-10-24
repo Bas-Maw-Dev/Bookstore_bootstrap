38ba2f630cafeeab8fe9dc1b4697544416099b92
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-10-24 15:58:37 +0100

    REFACTOR TEST: remove display authors, refactor alphabetical order

--- spec/features/Authors/authors_spec.rb
+++ spec/features/Authors/authors_spec.rb
@@ -14,17 +14,13 @@ RSpec.describe 'Author' do
     before do
       author1 = Author.create(first_name: 'William', second_name: 'Shakespeare')
       author2 = Author.create(first_name: 'Charles', second_name: 'Baudelaire')
+      author3 = Author.create(first_name: 'Jane', second_name: 'Austen')
       visit authors_path
     end
 
-    it 'displays the authors name' do
-      expect(page).to have_text('William Shakespeare')
-      expect(page).to have_text('Charles Baudelaire')
-    end
-
     it 'displays the authors in alphabetical order' do
-      expect(page).to have_selector('.author', count: 2)
-      expect(page.all('.author').map(&:text)).to eq(['Charles Baudelaire', 'William Shakespeare'])
+      expect(page).to have_selector('.author', count: 3)
+      expect(page.all('.author').map(&:text)).to eq(['Jane Austen', 'Charles Baudelaire', 'William Shakespeare'])
     end
   end
 end
