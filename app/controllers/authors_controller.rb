3c4c179eb5207aada128c3c51458f1f108f3c410
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-10-24 15:40:33 +0100

    test:authors are alphabetical order GREEN

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Author.all
+    @authors = Author.all.order(:second_name)
   end
   def show
     @author = Author.find(params[:id])
